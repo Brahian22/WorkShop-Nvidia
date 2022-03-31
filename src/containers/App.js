323f4de87d9a9140425c5bf1bdaa687f5d1fd83a
Brahian Alzate <brahianalzate02@gmail.com>
2022-03-31 16:01:09 -0500

    cardcomponent

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import CardComponent from '../components/CardComponent';
+
 
 function App() {
   return (
     <div className="App">
-
+      <CardComponent/>
     </div>
   );
 }
